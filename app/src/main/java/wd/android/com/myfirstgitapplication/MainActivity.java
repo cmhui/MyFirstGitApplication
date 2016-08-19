4be960babb7abc7860d925bd338e5a39220a5b3f
cmhui <1049803789@qq.com>
2016-08-19 21:11:17 +0800

    Initial commit

---
+++ app/src/main/java/wd/android/com/myfirstgitapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package wd.android.com.myfirstgitapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
